1c51373158614447da08022fea99c56e978bf11d
Jason Hite <jasonmhite@gmail.com>
2015-03-06 19:03:04 -0500

    Small change to post script

--- transmisson_post.sh
+++ transmisson_post.sh
@@ -4,5 +4,7 @@ BASEDIR="/root/.flexget/stat/success"
 
 # Check if the file exists
 if [[ -n "${TR_TORRENT_NAME}" && -a "${BASEDIR}/${TR_TORRENT_NAME}" ]]; then
-        echo "done" >> "${BASEDIR}/${TR_TORRENT_NAME}";
+        if [[ $(tail ${i} -n 1 | tr -d '\n') != "done" ]]; then
+                echo "done" >> "${BASEDIR}/${TR_TORRENT_NAME}";
+        fi 
 fi
