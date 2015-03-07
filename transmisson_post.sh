d1e4ad4d2265a01d5daebfd92e6354fc15e1865f
Jason Hite <jasonmhite@gmail.com>
2015-03-06 19:10:54 -0500

    Revert "Small change to post script"
    This reverts commit 1c51373158614447da08022fea99c56e978bf11d.

--- transmisson_post.sh
+++ transmisson_post.sh
@@ -4,7 +4,5 @@ BASEDIR="/root/.flexget/stat/success"
 
 # Check if the file exists
 if [[ -n "${TR_TORRENT_NAME}" && -a "${BASEDIR}/${TR_TORRENT_NAME}" ]]; then
-        if [[ $(tail ${i} -n 1 | tr -d '\n') != "done" ]]; then
-                echo "done" >> "${BASEDIR}/${TR_TORRENT_NAME}";
-        fi 
+        echo "done" >> "${BASEDIR}/${TR_TORRENT_NAME}";
 fi
